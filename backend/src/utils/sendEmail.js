5ac8cfa8dad0021dcf41f2ac5509294a1d6488d2
Ayush Agrawal <2305774@kiit.ac.in>
2026-06-20 00:47:32 +0530

    Fixed login, signup, OTP and image upload

--- backend/src/utils/sendEmail.js
+++ backend/src/utils/sendEmail.js
@@ -2,17 +2,12 @@ const nodemailer = require("nodemailer");
 
 const sendEmail = async (email, otp) => {
   const transporter = nodemailer.createTransport({
-    host: "smtp.gmail.com",
-    port: 465,
-    secure: true,
+    service: "gmail",
     auth: {
       user: process.env.EMAIL_USER,
       pass: process.env.EMAIL_PASS,
     },
   });
-
-  await transporter.verify();
-  console.log("SMTP Connected");
 
   await transporter.sendMail({
     from: process.env.EMAIL_USER,
